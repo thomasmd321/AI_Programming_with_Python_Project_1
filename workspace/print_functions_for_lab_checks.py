8081d3ea91d538539a908edb1d874eeb103460b2
Thomas Stewart <tstewa2@gmail.com>
2020-05-01 13:38:10 -0400

    if statements added for print_functions_for_lab_checks.py

--- workspace/print_functions_for_lab_checks.py
+++ workspace/print_functions_for_lab_checks.py
@@ -233,6 +233,11 @@ def check_calculating_results(results_dic, results_stats_dic):
      Nothing - just prints to console  
 
     """
+    #n_images = len(results_dic)
+    #n_pet_dog = 0
+    #n_class_cdog = 0
+    #n_class_cnotd = 0
+    #n_match_breed = 0
     if results_stats_dic is None:
         print("* Doesn't Check the Results Dictionary because 'calculates_results_stats' hasn't been defined.")
     else:
@@ -293,9 +298,18 @@ def check_calculating_results(results_dic, results_stats_dic):
                     
         # calculates statistics based upon counters from above
         n_pet_notd = n_images - n_pet_dog
-        pct_corr_dog = ( n_class_cdog / n_pet_dog )*100
-        pct_corr_notdog = ( n_class_cnotd / n_pet_notd )*100
-        pct_corr_breed = ( n_match_breed / n_pet_dog )*100
+        if n_pet_dog > 0:
+            pct_corr_dog = ( n_class_cdog / n_pet_dog )*100
+        else:
+            pct_corr_dog = 0
+        if n_pet_notd > 0:
+            pct_corr_notdog = ( n_class_cnotd / n_pet_notd )*100
+        else:
+            pct_corr_notdog = 0
+        if n_pet_dog > 0:
+            pct_corr_breed = ( n_match_breed / n_pet_dog )*100
+        else:
+            pct_corr_breed = 0
     
         # prints calculated statistics
         print("\n ** Statistics from calculates_results_stats() function:")
